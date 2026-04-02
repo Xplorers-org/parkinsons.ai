afbbd9f3165db41d773da840a304ccea8fed4bb3
UdasriHasindu <usrimart@gmail.com>
2026-04-02 06:22:39 +0530

    introduce favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,11 +13,11 @@ export const metadata: Metadata = {
   generator: 'v0.app',
   icons: {
     icon: [
-      { url: '/icon-light-32x32.png', media: '(prefers-color-scheme: light)' },
-      { url: '/icon-dark-32x32.png', media: '(prefers-color-scheme: dark)' },
-      { url: '/icon.svg', type: 'image/svg+xml' },
+      { url: '/favicon.ico', media: '(prefers-color-scheme: light)' },
+      { url: '/favicon.ico', media: '(prefers-color-scheme: dark)' },
+      { url: '/favicon.ico', type: 'image/svg+xml' },
     ],
-    apple: '/apple-icon.png',
+    apple: '/favicon.ico',
   },
 }
 
